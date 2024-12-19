899a0e340a11d6c898cdd833f7c1b9b143adeac5
ChanLee_KR <cl700120@gmail.com>
2024-12-19 17:29:50 +0900

    refactor: 블로그 메타데이터 layout 이전

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,19 +1,3 @@
-import type { Metadata } from 'next'
-import { BLOG_PAGE_TITLE } from '@/views/blog'
-
-export const metadata: Metadata = {
-  title: {
-    template: `%s - ${BLOG_PAGE_TITLE}`,
-    default: BLOG_PAGE_TITLE,
-  },
-  description: 'SI 퍼블리셔 출신 FE 개발자의 기술 블로그',
-  openGraph: {
-    type: 'website',
-    siteName: BLOG_PAGE_TITLE,
-    locale: 'ko_KR',
-  },
-}
-
 export default function BlogMainPage() {
   return <></>
 }
