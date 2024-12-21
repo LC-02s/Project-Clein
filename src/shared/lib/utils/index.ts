df3d04e9d5daa5092187ed02e810fb2273a82b5c
ChanLee_KR <cl700120@gmail.com>
2024-12-21 12:19:49 +0900

    feat: 접근성 관련 포커스 처리 유틸 함수 추가

--- src/shared/lib/utils/index.ts
+++ src/shared/lib/utils/index.ts
@@ -1,3 +1,6 @@
 export { default as cn } from './class-names'
 export { default as copyToClipboard } from './copy-to-clipboard'
 export type * from './get-time'
+export type * from './focusable-element'
+export * from './get-focusable-element'
+export { default as startFocusLoop } from './start-focus-loop'
---
+++ src/shared/lib/utils/start-focus-loop.ts
@@ -0,0 +1,30 @@
+import { getFocusableElementAll } from './get-focusable-element'
+
+export default function startFocusLoop<T extends HTMLElement>(element: T | null) {
+  const focusableEls = getFocusableElementAll(element)
+
+  if (focusableEls && focusableEls.length > 0) {
+    const firstFocusableEl = focusableEls[0]
+    const lastFocusableEl = focusableEls[focusableEls.length - 1]
+
+    firstFocusableEl?.addEventListener('keydown', (event: Event) => {
+      const e = event as KeyboardEvent
+
+      if (e.key === 'Tab' && e.shiftKey) {
+        e.preventDefault()
+        lastFocusableEl?.focus()
+      }
+    })
+
+    lastFocusableEl?.addEventListener('keydown', (event: Event) => {
+      const e = event as KeyboardEvent
+
+      if (e.key === 'Tab' && !e.shiftKey) {
+        e.preventDefault()
+        firstFocusableEl?.focus()
+      }
+    })
+
+    firstFocusableEl?.focus()
+  }
+}
