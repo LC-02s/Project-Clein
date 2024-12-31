b7a402091a65eedb41be33ccc983c0d4780a4cb9
ChanLee_KR <cl700120@gmail.com>
2024-12-31 17:05:13 +0900

    refactor: 공용 유틸 함수 export 순서 정리

--- src/shared/lib/utils/focusable-element.ts
+++
@@ -1,7 +0,0 @@
-export type FocusableElement =
-  | HTMLAnchorElement
-  | HTMLButtonElement
-  | HTMLInputElement
-  | HTMLTextAreaElement
-  | HTMLSelectElement
-  | HTMLDetailsElement
--- src/shared/lib/utils/get-focusable-element.ts
+++ src/shared/lib/utils/get-focusable-element.ts
@@ -1,4 +1,10 @@
-import type { FocusableElement } from './focusable-element'
+export type FocusableElement =
+  | HTMLAnchorElement
+  | HTMLButtonElement
+  | HTMLInputElement
+  | HTMLTextAreaElement
+  | HTMLSelectElement
+  | HTMLDetailsElement
 
 const focusableElementSelector =
   'button:not([disabled]), [href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), [tabindex]:not([tabindex="-1"]):not([disabled]), details:not([disabled]), summary:not(:disabled)'
