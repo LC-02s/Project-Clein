a8202b7c1352b3e58b5709cd633eb3a3fb9450b2
ChanLee_KR <cl700120@gmail.com>
2025-01-01 10:48:55 +0900

    fix: link 컴포넌트로 페이지 이동 시 밑줄이 튀는 현상 수정

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,9 +1,7 @@
-'use client'
-
 import { MainLogo } from '@/entities/site'
 import TabMenu from './tab-menu'
 
-export default function Header({ children }: React.PropsWithChildren) {
+export default async function Header({ children }: React.PropsWithChildren) {
   return (
     <header className="w-full pt-28 xl:pt-16">
       <div className="fixed inset-x-0 top-0 z-50 flex h-28 w-full flex-col items-start justify-start border-b border-zinc-200 bg-white xl:h-auto dark:border-zinc-700 dark:bg-zinc-800">
--- src/widgets/header/ui/tab-underline.tsx
+++ src/widgets/header/ui/tab-underline.tsx
@@ -7,7 +7,7 @@ export default function TabUnderline() {
     <motion.div
       layoutId="header/tab-menu"
       className="absolute inset-x-0 bottom-0 h-0.5 bg-indigo-500 dark:bg-indigo-300"
-      transition={{ type: 'spring', stiffness: 480, damping: 32 }}
+      transition={{ ease: 'easeOut', duration: 0.2 }}
       layout
     />
   )
