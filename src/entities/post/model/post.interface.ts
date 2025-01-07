875279ba4813c32c4bd4753f5adfd80de14c2bbb
ChanLee_KR <cl700120@gmail.com>
2025-01-07 14:40:22 +0900

    refactor: 날짜 정렬 기능 확장

--- src/entities/post/model/post.interface.ts
+++ src/entities/post/model/post.interface.ts
@@ -1,19 +1,17 @@
 import type { KeywordRepository } from '@/database/posts'
-import type { LiteralDate, LiteralDateTime } from '@/shared/lib'
+import type { LiteralDate, SortedFromDate } from '@/shared/lib'
 
 export type Keyword = ReturnType<typeof KeywordRepository.getKeys>[number]
 
 export type PostId = LiteralDate
 
-export interface PostRawData {
+export interface PostRawData extends SortedFromDate {
   title: string
   description: string
   thumbnail: {
     src: `/${string}.${'png' | 'jpeg' | 'jpg' | 'gif'}`
     alt: string
   }
-  createdAt: LiteralDateTime
-  updatedAt: LiteralDateTime
   keywords: Keyword[]
   externalTags?: string[]
 }
--- src/shared/lib/utils/sort-by-date.ts
+++ src/shared/lib/utils/sort-by-date.ts
@@ -2,6 +2,19 @@ import { type LiteralDateTime, getTime } from './get-time'
 
 export type SortType = 'asc' | 'desc'
 
+export type SortedFromDateKey = 'createdAt' | 'updatedAt'
+
+export type SortedFromDate = Record<SortedFromDateKey, LiteralDateTime>
+
+export interface SortedInfo {
+  sorted: {
+    from: SortedFromDateKey
+    at: SortType
+  }
+}
+
+export const SORT_PARAMS_SEPARATOR = ','
+
 export function orderByDateDesc(a: LiteralDateTime, b: LiteralDateTime) {
   return getTime(b) - getTime(a)
 }
@@ -10,6 +23,21 @@ export function orderByDateAsc(a: LiteralDateTime, b: LiteralDateTime) {
   return getTime(a) - getTime(b)
 }
 
-export function sortByDate(sortType?: SortType) {
-  return sortType === 'asc' ? orderByDateAsc : orderByDateDesc
+export function sortByDate<T extends SortedFromDate>(
+  params?: string | null,
+): SortedInfo & { compare: (a: T, b: T) => number } {
+  const sortParams = params?.split(SORT_PARAMS_SEPARATOR)
+  const [sortedFrom, sortType]: [SortedFromDateKey, SortType] = sortParams
+    ? [
+        sortParams[0] === 'updatedAt' ? 'updatedAt' : 'createdAt',
+        sortParams[1] === 'asc' ? 'asc' : 'desc',
+      ]
+    : ['createdAt', 'desc']
+
+  const compareFn = sortType === 'asc' ? orderByDateAsc : orderByDateDesc
+
+  return {
+    sorted: { from: sortedFrom, at: sortType },
+    compare: (a, b) => compareFn(a[sortedFrom], b[sortedFrom]),
+  }
 }
