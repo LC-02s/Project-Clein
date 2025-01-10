fbcdacd643858d02fb55b4735423c417392f0045
ChanLee_KR <cl700120@gmail.com>
2025-01-10 19:38:36 +0900

    refactor: dimmed 배경색 스타일 분리

--- src/shared/ui/dimmed.tsx
+++ src/shared/ui/dimmed.tsx
@@ -10,10 +10,7 @@ export default function Dimmed({ className, ...props }: DimmedProps) {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ ease: 'easeInOut', duration: 0.16 }}
-      className={cn(
-        'absolute inset-0 -z-[1] m-0 border-none bg-black/30 p-0 dark:bg-slate-200/20',
-        className,
-      )}
+      className={cn('bg-dimmed absolute inset-0 -z-[1] m-0 border-none p-0', className)}
       {...props}
     />
   )
