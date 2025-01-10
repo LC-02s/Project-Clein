835f1b82f6181f7b1c5d63e696c7fd27c0b10c36
ChanLee_KR <cl700120@gmail.com>
2025-01-10 16:03:05 +0900

    feat: 공용 지연 렌더링 컴포넌트 추가

---
+++ src/shared/ui/deferred.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import { useTimeout } from '../lib'
+
+export default function Deferred({ children }: React.PropsWithChildren) {
+  const { start } = useTimeout()
+  const [isDeferred, setIsDeferred] = useState(false)
+
+  useEffect(() => {
+    start(() => setIsDeferred(true), 200)
+  }, [start])
+
+  if (!isDeferred) {
+    return null
+  }
+
+  return <>{children}</>
+}
