a62f5faf3deaa3e7fc8f3983457fa52652dd3630
ChanLee_KR <cl700120@gmail.com>
2025-02-02 01:01:57 +0900

    feat: 블로그 메인 페이지 추가

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,66 @@
-import { UnderConstruction } from '@/shared/ui'
+import { BlogLayout, KeywordsDrawerTrigger } from '@/widgets/blog-layout'
+import { PageController } from '@/widgets/page-controller'
+import { PostKeyword } from '@/widgets/post-keyword'
+import { PostList } from '@/widgets/post-list'
+import { Profile, ProfileSkeleton } from '@/widgets/profile'
+import { SortDropdownButton } from '@/features/change-sort-type'
+import { LinkWithLoader } from '@/features/loader'
+import { type PostListParamsKey, getPostList, POST_LIST_PARAMS } from '@/entities/post'
+import type { SearchParams } from '@/shared/api'
+import { BLOG_PATH } from '@/shared/config'
+import {
+  createSearchParamsFilter,
+  joinSortParams,
+  DEFAULT_PAGE_SIZE,
+  DEFAULT_SORT_PARAMS,
+} from '@/shared/lib'
 
-export default function BlogMainPage() {
-  return <UnderConstruction />
+interface BlogMainPageProps {
+  searchParams: Promise<SearchParams>
 }
+
+const BlogMainPage: React.FC<BlogMainPageProps> = async ({ searchParams }) => {
+  const { page, contents, sorted, keyword } = await getPostList(await searchParams)
+  const sortParams = joinSortParams(sorted.from, sorted.at)
+
+  const href = createSearchParamsFilter<PostListParamsKey>({
+    params: [
+      [POST_LIST_PARAMS.PAGE, page.current],
+      [POST_LIST_PARAMS.SIZE, page.size, DEFAULT_PAGE_SIZE],
+      [POST_LIST_PARAMS.SORT, sortParams, DEFAULT_SORT_PARAMS],
+      [POST_LIST_PARAMS.KEYWORD, keyword],
+    ],
+    pathname: BLOG_PATH,
+  })
+
+  return (
+    <BlogLayout
+      profile={<Profile />}
+      profileFallback={ProfileSkeleton}
+      keywords={
+        <PostKeyword
+          current={keyword}
+          baseURL={href([POST_LIST_PARAMS.KEYWORD, POST_LIST_PARAMS.PAGE])}
+        />
+      }
+    >
+      <PostList contents={contents} length={page.length} sortedFrom={sorted.from}>
+        <KeywordsDrawerTrigger active={!!keyword} />
+        <SortDropdownButton<PostListParamsKey>
+          sortParams={sortParams}
+          paramsKey={POST_LIST_PARAMS.SORT}
+          baseURL={href([POST_LIST_PARAMS.SORT])}
+          renderLink={LinkWithLoader}
+          className="-right-2"
+        />
+      </PostList>
+      <PageController
+        page={page}
+        baseURL={href([POST_LIST_PARAMS.PAGE])}
+        className="mt-8 border-t pl-0.5 pt-6"
+      />
+    </BlogLayout>
+  )
+}
+
+export default BlogMainPage
