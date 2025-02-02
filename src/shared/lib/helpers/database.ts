2b8504353a3d24c29fe0e4a12f621b305168285d
ChanLee_KR <cl700120@gmail.com>
2025-02-02 15:21:23 +0900

    fix: 데이터베이스 경로 접근 방식 변경

--- src/shared/lib/helpers/database.ts
+++ src/shared/lib/helpers/database.ts
@@ -1,11 +1,11 @@
 'use server'
 
 import { readFile } from 'fs/promises'
-import { resolve } from 'path'
+import { join } from 'path'
 
 export const readFileToDatabase = async (path: string) => {
   const DATABASE_PATH = '__database__' as const
-  const contentPath = resolve(`${DATABASE_PATH}${path}`)
+  const contentPath = join(process.cwd(), `${DATABASE_PATH}${path}`)
 
   return await readFile(contentPath, 'utf-8')
 }
