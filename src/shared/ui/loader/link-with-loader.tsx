1f373c466ee208b15afeef52cc6199b3e3ff27e7
ChanLee_KR <cl700120@gmail.com>
2025-02-16 01:10:12 +0900

    design: 로딩 바 최댓값 지정 및 오동작 수정

--- src/shared/ui/loader/link-with-loader.tsx
+++ src/shared/ui/loader/link-with-loader.tsx
@@ -4,17 +4,14 @@ import Link from 'next/link'
 import { useLoaderSwitch } from '../../lib'
 
 export const LinkWithLoader: typeof Link = (({ href, onClick, children, ...props }) => {
-  const { on } = useLoaderSwitch()
+  const { on, off } = useLoaderSwitch()
 
   return (
     <Link
       href={href}
       onClick={(e) => {
-        if (
-          typeof href === 'string' &&
-          href !== `${window.location.pathname}${window.location.search}`
-        ) {
-          on()
+        if (typeof href === 'string') {
+          ;(href !== `${location.pathname}${location.search}` ? on : off)()
         }
 
         onClick?.(e)
