d41b159289b2f736049dc40ded27fba16f5628ee
ChanLee_KR <cl700120@gmail.com>
2025-02-18 01:04:03 +0900

    refactor: 코멘트 컴포넌트 테마 check hydration 훅 적용

--- src/shared/ui/comment.tsx
+++ src/shared/ui/comment.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from 'react'
 import { GITHUB } from '../config'
-import { type Theme, useTheme, useTimeout, cn } from '../lib'
+import { type Theme, useTheme, cn, useCheckHydration } from '../lib'
 
 const GISCUS_ADDRESS = 'https://giscus.app'
 const GISCUS_CLASS_NAME = 'giscus'
@@ -21,44 +21,42 @@ const changeGiscusTheme = (theme: Theme) => {
 export type CommentProps = Omit<React.JSX.IntrinsicElements['div'], keyof React.PropsWithChildren>
 
 export const Comment: React.FC<CommentProps> = ({ className, ...props }) => {
-  const { start } = useTimeout()
   const { theme } = useTheme()
+  const isHydrated = useCheckHydration()
 
   useEffect(() => {
     const id = `${GISCUS_CLASS_NAME}-client-loader`
     const failed = !changeGiscusTheme(theme)
 
-    if (failed) {
-      start(() => {
-        const scriptEl = document.createElement('script')
+    if (isHydrated && failed) {
+      const scriptEl = document.createElement('script')
 
-        scriptEl.id = id
-        scriptEl.src = `${GISCUS_ADDRESS}/client.js`
-        scriptEl.async = true
-        scriptEl.crossOrigin = 'anonymous'
+      scriptEl.id = id
+      scriptEl.src = `${GISCUS_ADDRESS}/client.js`
+      scriptEl.async = true
+      scriptEl.crossOrigin = 'anonymous'
 
-        scriptEl.setAttribute('data-repo', `${GITHUB.USER_ID}/${GITHUB.REPO.NAME}`)
-        scriptEl.setAttribute('data-repo-id', GITHUB.REPO.ID)
-        scriptEl.setAttribute('data-category', GITHUB.REPO.CATEGORY.NAME)
-        scriptEl.setAttribute('data-category-id', GITHUB.REPO.CATEGORY.ID)
-        scriptEl.setAttribute('data-mapping', 'pathname')
-        scriptEl.setAttribute('data-strict', '0')
-        scriptEl.setAttribute('data-reactions-enabled', '1')
-        scriptEl.setAttribute('data-emit-metadata', '1')
-        scriptEl.setAttribute('data-input-position', 'top')
-        scriptEl.setAttribute('data-theme', theme)
-        scriptEl.setAttribute('data-lang', 'ko')
-        scriptEl.setAttribute('data-loading', 'lazy')
+      scriptEl.setAttribute('data-repo', `${GITHUB.USER_ID}/${GITHUB.REPO.NAME}`)
+      scriptEl.setAttribute('data-repo-id', GITHUB.REPO.ID)
+      scriptEl.setAttribute('data-category', GITHUB.REPO.CATEGORY.NAME)
+      scriptEl.setAttribute('data-category-id', GITHUB.REPO.CATEGORY.ID)
+      scriptEl.setAttribute('data-mapping', 'pathname')
+      scriptEl.setAttribute('data-strict', '0')
+      scriptEl.setAttribute('data-reactions-enabled', '1')
+      scriptEl.setAttribute('data-emit-metadata', '1')
+      scriptEl.setAttribute('data-input-position', 'top')
+      scriptEl.setAttribute('data-theme', theme)
+      scriptEl.setAttribute('data-lang', 'ko')
+      scriptEl.setAttribute('data-loading', 'lazy')
 
-        document.getElementById(id)?.remove()
-        document.body.appendChild(scriptEl)
-      }, 300)
+      document.getElementById(id)?.remove()
+      document.body.appendChild(scriptEl)
     }
 
     return () => {
       document.getElementById(id)?.remove()
     }
-  }, [theme, start])
+  }, [theme, isHydrated])
 
   return <div className={cn(GISCUS_CLASS_NAME, className)} {...props} />
 }
