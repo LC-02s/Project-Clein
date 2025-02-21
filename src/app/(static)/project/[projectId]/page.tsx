882e64113285b028151af2ebad9cc0e422195929
ChanLee_KR <cl700120@gmail.com>
2025-02-22 00:31:39 +0900

    feat: 프로젝트 상세 페이지 추가

---
+++ src/app/(static)/project/[projectId]/page.tsx
@@ -0,0 +1,85 @@
+import Image from 'next/image'
+import type { Metadata } from 'next'
+import { ProjectRepository } from '@/database/projects'
+import { ContentBody } from '@/widgets/content'
+import { getPostList, POST_LIST_PARAMS } from '@/entities/post'
+import { type ProjectId, PROJECT_TYPE_LABEL } from '@/entities/project'
+import { getMarkdownContent } from '@/shared/api'
+import { BLOG_PATH, THUMBNAIL_SIZE } from '@/shared/config'
+import { extractImageType } from '@/shared/lib'
+import { NotFound } from '@/shared/ui'
+
+interface ProjectDetailParams {
+  projectId: ProjectId
+}
+
+interface ProjectDetailPageProps {
+  params: Promise<ProjectDetailParams>
+}
+
+export const generateStaticParams = () =>
+  ProjectRepository.getKeys().map<ProjectDetailParams>((key) => ({ projectId: key }))
+
+export const generateMetadata = async ({ params }: ProjectDetailPageProps): Promise<Metadata> => {
+  const { projectId } = await params
+  const project = ProjectRepository.findById(projectId)
+
+  if (!project) {
+    return {}
+  }
+
+  return {
+    title: `${project.name} 프로젝트 소개`,
+    openGraph: {
+      images: {
+        url: project.thumbnail.src,
+        alt: project.thumbnail.alt,
+        type: extractImageType(project.thumbnail.src),
+        ...THUMBNAIL_SIZE,
+      },
+    },
+  }
+}
+
+const ProjectDetailPage: React.FC<ProjectDetailPageProps> = async ({ params }) => {
+  const { projectId } = await params
+  const project = ProjectRepository.findById(projectId)
+
+  if (!project) {
+    return <NotFound />
+  }
+
+  const [data, content] = await Promise.all([
+    getPostList({ [POST_LIST_PARAMS.KEYWORD]: projectId }),
+    getMarkdownContent(`/projects/${projectId}`).catch(() => ''),
+  ])
+
+  const { name, description, period, githubURL, serviceURL, iconURL } = project
+  const type = PROJECT_TYPE_LABEL[project.type]
+  const posts = data.contents.reduce((text, { id, title }) => {
+    return (text += `- <a href="${BLOG_PATH}/${id}">${title}</a><br />\n`)
+  }, '\n')
+
+  return (
+    <div className="wrapper-xl py-screen">
+      <h2 className="relative mb-12 flex items-center justify-center whitespace-nowrap p-4 pr-8 text-center text-2xl font-bold md:text-3xl">
+        <Image
+          src={iconURL}
+          alt={`${name} 프로젝트 아이콘`}
+          width={36}
+          height={36}
+          className="mr-3 size-7 drop-shadow md:size-9"
+          priority
+        />
+        {name}
+      </h2>
+      <ContentBody
+        content={content}
+        mapper={{ name, description, type, period, githubURL, serviceURL, posts }}
+        className="pt-0"
+      />
+    </div>
+  )
+}
+
+export default ProjectDetailPage
---
+++ src/app/(static)/project/layout.tsx
@@ -0,0 +1,46 @@
+import Image from 'next/image'
+import { BackgroundGrid } from '@/shared/ui'
+
+const ProjectLayout: React.FC<React.PropsWithChildren> = ({ children }) => (
+  <>
+    <BackgroundGrid position="top" className="h-96">
+      <Image
+        src="/images/retro-futuristic-3d-mesh-circle.png"
+        alt="구 형태 3D 메쉬 오브젝트"
+        className="pointer-events-none absolute left-[-12%] top-[-16%] max-w-60 select-none max-md:w-1/2 md:max-w-full xl:left-0 xl:top-[-36%] 2xl:left-[2%]"
+        width={360}
+        height={360}
+        quality={100}
+        priority
+      />
+      <Image
+        src="/images/retro-futuristic-3d-mesh-multiple.png"
+        alt="곱하기 기호 형태 3D 메쉬 오브젝트"
+        className="pointer-events-none absolute right-[-4%] top-[32%] max-w-40 select-none max-md:w-1/3 md:right-[2%] md:top-[24%] md:max-w-full xl:top-[36%]"
+        width={200}
+        height={108}
+        quality={100}
+        priority
+      />
+    </BackgroundGrid>
+    {children}
+    <BackgroundGrid />
+  </>
+)
+
+/*
+<div className="pointer-events-none absolute inset-x-0 bottom-0 z-[-1] h-full overflow-hidden">
+  <Image
+    src="/images/retro-futuristic-3d-mesh-wave.png"
+    alt="파동 형태 3D 메쉬 오브젝트"
+    className="pointer-events-none absolute -bottom-8 right-4 w-[45rem] min-w-96 max-w-[120vw] select-none"
+    style={{ opacity: 0.6 }}
+    width={720}
+    height={540}
+    quality={100}
+    priority
+  />
+</div>
+*/
+
+export default ProjectLayout
