eaa541d3b94312f1522524b746a753c791bf18ec
ChanLee_KR <cl700120@gmail.com>
2025-03-06 00:36:18 +0900

    refactor: fallback render 컴포넌트 적용

--- src/widgets/content/ui/content-body.tsx
+++ src/widgets/content/ui/content-body.tsx
@@ -1,5 +1,6 @@
 import { MDXRemote } from 'next-mdx-remote/rsc'
 import { type PropsWithClassName, cn, createPolymorphicComponent } from '@/shared/lib'
+import { FallbackRender } from '@/shared/ui'
 import { options } from '../config'
 import { type ContentMapper, parseHTMLContent } from '../lib'
 import { EmptyContent } from './empty-content'
@@ -13,12 +14,8 @@ export interface ContentParserProps extends PropsWithClassName {
 export const ContentBody = createPolymorphicComponent<
   ContentParserProps,
   Omit<React.JSX.IntrinsicElements['div'], keyof React.PropsWithChildren>
->(async ({ content, mapper, className, component: Component = 'div', ...props }) => {
-  if (!content) {
-    return <EmptyContent />
-  }
-
-  return (
+>(({ content, mapper, className, component: Component = 'div', ...props }) => (
+  <FallbackRender render={!content} component={<EmptyContent />}>
     <Component
       className={cn('py-12 text-base text-gray-700 md:text-lg dark:text-gray-200', className)}
       {...props}
@@ -29,5 +26,5 @@ export const ContentBody = createPolymorphicComponent<
         source={parseHTMLContent(content, htmlComponents, mapper)}
       />
     </Component>
-  )
-})
+  </FallbackRender>
+))
