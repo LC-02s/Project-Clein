0ca1babb39c254051beb1026e666a1751d6ca306
ChanLee_KR <cl700120@gmail.com>
2025-03-07 00:04:11 +0900

    refactor: 타이틀 속성 확장

--- src/widgets/content/ui/link-text.tsx
+++ src/widgets/content/ui/link-text.tsx
@@ -5,30 +5,26 @@ import { ExternalLink, LinkWithLoader } from '@/shared/ui'
 
 export interface LinkTextProps extends React.PropsWithChildren<PropsWithClassName> {
   href: string
+  title?: string
 }
 
-export const LinkText: React.FC<LinkTextProps> = ({ href, className, children }) => {
+export const LinkText: React.FC<LinkTextProps> = ({ href, title = '', className, children }) => {
   const classNames = cn('font-medium text-blue-700 hover:underline dark:text-blue-300', className)
+  const content = typeof children === 'string' ? children : ''
 
   if (href.startsWith('http')) {
     return (
-      <ExternalLink
-        href={href}
-        title={typeof children === 'string' ? children : ''}
-        className={classNames}
-      >
+      <ExternalLink href={href} title={title || content} className={classNames}>
         {children}
       </ExternalLink>
     )
   }
 
-  const title = typeof children === 'string' ? `: ${children}` : ''
-
   if (href.startsWith('#')) {
     return (
       <a
         href={href}
-        title={`영역 이동${title}`}
+        title={`영역 이동${title ? `: ${title}` : content && `: ${content}`}`}
         className={classNames}
         onClick={createScrollToSection(href)}
       >
@@ -38,7 +34,11 @@ export const LinkText: React.FC<LinkTextProps> = ({ href, className, children })
   }
 
   return (
-    <LinkWithLoader href={href} title={`페이지 이동${title}`} className={classNames}>
+    <LinkWithLoader
+      href={href}
+      title={`페이지 이동${title ? `: ${title}` : content && `: ${content}`}`}
+      className={classNames}
+    >
       {children}
     </LinkWithLoader>
   )
